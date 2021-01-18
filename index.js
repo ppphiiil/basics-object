4b9121757c91668f9465aa50a0380f00851ce215
ppphiiil <info@philmeinwelt.com>
2021-01-18 15:28:06 +0100

    formatting

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 //*****************  EXERCISES 1
 console.log(`\n\nEXERCISE 1`);
 // # Object like array
-// ​
 // Use object literal syntax (e.g., `{ key: value, ... }` notation) to create an object that behaves as an array in a for statement. The object should contain at least 3 elements. You should place your code between the braces in the let statement:
-// ​
-// ```js
+
 let myArray = {
     "length": 3,
     0: "val",
@@ -24,11 +22,8 @@ for (let i = 0; i < myArray.length; i += 1) {
 console.log(`\n\nEXERCISE 2`);
 
 // # Empty object
-// ​
 // Using JavaScript, check if the object is empty and print the result to the console.
-// ​
 // {} ➞ true
-// ​
 // { a: 1 } ➞ false
 
 
@@ -94,7 +89,6 @@ let arrayWithKeys = [];
 for (let i in testObject2) { //! interate through the properties
     arrayWithKeys.push(i); //! returns the keys of the onject
     arrayWithValues.push(testObject2[i]); //! returns the content of an object
-
 }
 
 console.log(arrayWithValues);
