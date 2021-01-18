625f951c45b92afea449bcf92222b9670994f07e
ppphiiil <info@philmeinwelt.com>
2021-01-18 16:01:51 +0100

    next exercise

--- index.js
+++ index.js
@@ -93,3 +93,88 @@ for (let i in testObject2) { //! interate through the properties
 
 console.log(arrayWithValues);
 console.log(arrayWithKeys);
+
+
+
+//********************************************
+//*****************  EXERCISES 4
+console.log(`\n\nEXERCISE 4`);
+
+// # Props count
+// ​
+// Using JavaScript, find the object that has highest number of properties. You need to count only the top level properties.
+// Print to the console the object with the highest number of properties and its number of properties.
+// ​
+
+
+const obj1 = {
+    name: 'Jane',
+    age: 21,
+    isEnrolled: true,
+    courses: [],
+    address: {
+        street: 'Boddinstr',
+        houseNumber: 21,
+        city: 'Berlin'
+    },
+    grades: [],
+
+};
+const obj2 = {
+    price: 1222,
+    categories: [{
+        id: 32,
+        slug: 'clothing',
+        label: 'Apparel',
+    }],
+};
+const obj3 = {
+    title: '',
+    author: '',
+    createdAt: '',
+    dateLastUpdated: '',
+    isApproved: false,
+};
+const obj4 = {
+    albumId: 1,
+    id: 26,
+    title: "asperiores nobis voluptate qui",
+    url: "https://via.placeholder.com/600/474645",
+    thumbnailUrl: "https://via.placeholder.com/150/474645",
+};
+const obj5 = {
+    postId: 1,
+    id: 1,
+    userId: 1,
+    name: "id labore ex et quam laborum",
+    email: "Eliseo@gardner.biz",
+    title: "quidem molestiae enim",
+    body: "laudantium enim quasi est quidem magnam voluptate ipsam eos tempora quo necessitatibus dolor quam autem quasi reiciendis et nam sapiente accusantium"
+};
+
+let array = [obj1, obj2, obj3, obj4, obj5];
+let counter = 0;
+
+let biggestKeyCount = 0;
+let biggestObject = {};
+
+
+
+for (let object of array) { //! take the first object of the array
+    counter = 0;
+    for (let props in object) { //! take the first key of the object
+
+        if (object.hasOwnProperty(props)) { //! if there is a key, conter +1
+            counter++;
+        }
+    }
+    if (biggestKeyCount < counter) { //! save the object with the highest number of keys
+        biggestKeyCount = counter;
+        biggestObject = object; //! save the biggest object
+
+    }
+    console.log("counter:" + counter);
+}
+console.log(`This is the biggest Object and it has ${counter} keys`);
+console.log(biggestObject);
+
